8ec1af806c63002f20d5507ffcf29c518841bc66
lalutskie <krizoledan@gmail.com>
2026-03-04 19:32:11 +0800

    updated new canva link

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,7 +59,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "873c91d663986719e2098e1e7f06db4f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "0f61110d4dae571c9fe708e3490116b1",
+"flutter_bootstrap.js": "6765ee92a4887558a9877cd39ee4fb25",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
