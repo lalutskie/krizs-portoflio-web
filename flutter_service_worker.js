b73acd97a6cca772f25709cbc1c0d515a32417e2
lalutskie <krizoledan@gmail.com>
2026-03-04 19:27:42 +0800

    update the portfolio canva link

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,14 +59,22 @@ const RESOURCES = {"assets/AssetManifest.bin": "873c91d663986719e2098e1e7f06db4f
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
+<<<<<<< HEAD
 "flutter_bootstrap.js": "f897a778548f7278b580b916529a06ad",
+=======
+"flutter_bootstrap.js": "0f61110d4dae571c9fe708e3490116b1",
+>>>>>>> 5e0318e (updated the canva link)
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "37dc1aa59e6c048cd3f9aecc64c09fd1",
 "/": "37dc1aa59e6c048cd3f9aecc64c09fd1",
+<<<<<<< HEAD
 "main.dart.js": "bfb3c8057b3fbcfcff6da8d3bf904fda",
+=======
+"main.dart.js": "70aaddc015dc664300efcdaf4112e5b7",
+>>>>>>> 5e0318e (updated the canva link)
 "manifest.json": "3a4a87c28cf0d010cd6eb85ecdd0499c",
 "version.json": "a02e1ec53b5afac95daafbcf67d670de"};
 // The application shell files that are downloaded before a service worker can
